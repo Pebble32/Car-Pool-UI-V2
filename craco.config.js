c88366c6aeccaf1a1f5012f829ef0985e68d8d06
MatioS <dkm2@hi.is>
2024-11-17 19:10:55 +0000

    Fix buiild error

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,32 @@
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   webpack: {
-    plugins: [new NodePolyfillPlugin()],
+    configure: (webpackConfig) => {
+      const fallback = webpackConfig.resolve.fallback || {};
+      Object.assign(fallback, {
+        "assert": require.resolve("assert"),
+        "buffer": require.resolve("buffer"),
+        "console": require.resolve("console-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "process": require.resolve("process/browser"),
+        "stream": require.resolve("stream-browserify"),
+        "url": require.resolve("url"),
+        "querystring": require.resolve("querystring-es3"), // Add this line
+      });
+      webpackConfig.resolve.fallback = fallback;
+
+      webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser',
+        }),
+      ]);
+
+      return webpackConfig;
+    },
   },
 };
