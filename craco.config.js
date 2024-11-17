cd141c6fdbddb556c651bf81ba2c98c6a8b0d4ac
MatioS <dkm2@hi.is>
2024-11-17 19:14:31 +0000

    Fix

--- craco.config.js
+++ craco.config.js
@@ -3,21 +3,19 @@ const webpack = require('webpack');
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      const fallback = webpackConfig.resolve.fallback || {};
-      Object.assign(fallback, {
-        "assert": require.resolve("assert"),
-        "buffer": require.resolve("buffer"),
-        "console": require.resolve("console-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "process": require.resolve("process/browser"),
-        "stream": require.resolve("stream-browserify"),
-        "url": require.resolve("url"),
-        "querystring": require.resolve("querystring-es3"), // Add this line
-      });
-      webpackConfig.resolve.fallback = fallback;
+      webpackConfig.resolve.fallback = {
+        assert: 'assert',
+        buffer: 'buffer',
+        console: 'console-browserify',
+        crypto: 'crypto-browserify',
+        http: 'stream-http',
+        https: 'https-browserify',
+        os: 'os-browserify/browser',
+        process: 'process/browser',
+        stream: 'stream-browserify',
+        url: 'url',
+        querystring: 'querystring-es3',
+      };
 
       webpackConfig.plugins = (webpackConfig.plugins || []).concat([
         new webpack.ProvidePlugin({
